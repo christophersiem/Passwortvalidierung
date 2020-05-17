6b038c76d7b8b7bd9b5f050ef6a740f2648f2c0b
christophersiem <christopher.siem@gmail.com>
2020-05-17 21:08:04 +0200

    Tested

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 
     public static void main(String[] args) {
 
-        String password = "123ab321";
+        String password = "123";
 
         System.out.println(Passwortvalidierung.pwcheck8zeichen(password));
         System.out.println(Passwortvalidierung.pwCheckEineZahl(password));
